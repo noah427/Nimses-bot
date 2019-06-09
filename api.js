0a4d406e221db5e0b7264296b794ecf9ec929419
Redacted <40678304+noah427@users.noreply.github.com>
2019-06-08 21:58:53 -0400

    bruh

--- api.js
+++ api.js
@@ -3,6 +3,8 @@ var request = require('request')
 var exports = module.exports = {}
 
 exports.getUserInfo = function(name, callback){
+  
+name = name.toLowerCase()
 
 request(`https://web.nimses.com/api/profile/${name}`, function (error, response, body) {
 if(response.statusCode == 404){
