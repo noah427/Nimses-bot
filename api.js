3db6e1c0079f19b2402e76ce25b818e70b11b9f3
Redacted <40678304+noah427@users.noreply.github.com>
2019-06-10 16:10:03 -0400

    Update api.js

--- api.js
+++ api.js
@@ -2,18 +2,36 @@ var request = require('request')
 
 var exports = module.exports = {}
 
-exports.getUserInfo = function(name, callback){
-  
-name = name.toLowerCase()
+exports.getUserInfo = function(name, callback) {
+  name = name.toLowerCase()
 
-request(`https://web.nimses.com/api/profile/${name}`, function (error, response, body) {
-if(response.statusCode == 404){
-var Userinfo = 404
-}
-else{
-var Userinfo = JSON.parse(body)
-}
-callback(Userinfo)
+  request(`https://web.nimses.com/api/profile/${name}`, function(error, response, body) {
+    if (response.statusCode == 404) {
+      var Userinfo = 404
+    }
+    else {
+      var Userinfo = JSON.parse(body)
+    }
+    callback(Userinfo)
 
-});
+  });
+}
+
+exports.getUserPosts = function(name,cb){
+
+    request(`https://web.nimses.com/api/profile/${name}`, function(error, response, body) {
+      console.log(response)
+    if (response.statusCode == 404) {
+      var Userinfo = 404
+    }
+    else {
+      var Userinfo = JSON.parse(body)
+        request(`https://web.nimses.com/api/feed/user?id=${Userinfo.id}&cursor=&limit=3`,function(response,err,body){
+          var posts = JSON.parse(body)
+          console.log(posts)
+          cb(posts)
+       })
+    }
+    
+  });
 }
