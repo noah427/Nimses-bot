828c51d6fa42da55dc312bbdbbaf370eff4ee347
Redacted <40678304+noah427@users.noreply.github.com>
2019-06-10 16:44:28 -0400

    Update api.js

--- api.js
+++ api.js
@@ -20,15 +20,13 @@ exports.getUserInfo = function(name, callback) {
 exports.getUserPosts = function(name,cb){
 
     request(`https://web.nimses.com/api/profile/${name}`, function(error, response, body) {
-      console.log(response)
     if (response.statusCode == 404) {
       var Userinfo = 404
     }
     else {
       var Userinfo = JSON.parse(body)
-        request(`https://web.nimses.com/api/feed/user?id=${Userinfo.id}&cursor=&limit=3`,function(response,err,body){
+        request(`https://web.nimses.com/api/feed/user?id=${Userinfo.profile.id}&cursor=&limit=3`,function(response,err,body){
           var posts = JSON.parse(body)
-          console.log(posts)
           cb(posts)
        })
     }
