ce2a46e520abdfcb51e0617747c7c03fb3c90fca
noah feinberg <redacted427@gmail.com>
2019-08-16 20:45:12 -0400

    place config in its own file

--- bot/app.js
+++ bot/app.js
@@ -4,68 +4,7 @@ const { inspect } = require('util');
 var utilities = require('./commands/utilities')
 const dotenv = require(`dotenv`).config()
 const apiCommands = require('./commands/apiCommands')
-
-
-
-var config = {
-  prefix: "?",
-  ownerID : "450429165200736256",
-  partneredServers: [
-    "593210721517699083",
-    "591039202913812480",
-    "589825043777847297",
-    "586853359474966557",
-  ],
-  helpMessage: `
-  ?user <username> : shows user info
-  ?limit <username> : shows your nim spending limit and how much you have spent
-  ?posts <username> : shows your posts
-  ?profile <username> : links your profile for easy nom4nom
-  ?info : about + repo + server link
-  ?help : shows this message
-  ?global : shows global nimses info
-  ?servers : shows which servers the bot is in
-  ?statusrole <username> : gives you the role for the status you have on nimses partnered server only
-  ?templestop : coming very soon will show top temples info
-  
-  -----------------
-  all of these ^ commands can be used without the username
-  if your nickname is your nimses username!
-
-  -----------------
-
-  Creator : [REDACTED]#1227
-  `,
-  infoMessage: `
-  Repo : https://github.com/noah427/nimses-bot
-  Server : https://discord.gg/45zHN9Y
-
-  ------------------------------------------
-  
-  About : 
-  Nim-Master shows nimses stats such as daily nim transfer limit
-  and how many nims a user has in addition to
-  many other useful things such as easy nom4nom profile link
-  and showing a users three most recent posts!
-
-  -------------------------------------------
-
-  ?help for commands list
-
-  -------------------------------------------
-
-  Creator : [REDACTED]#1227
-  `,
-  rolesToMake : [
-    ["icon","RED"],
-    ["angel", "#ff0066"],
-    ["indie","ORANGE"],
-    ["persona","#ffcc00"],
-    ["human","GREEN"],
-    ["user","GREY"],
-  ],
-}
-
+var config = require('../config.json')
 
 
 
